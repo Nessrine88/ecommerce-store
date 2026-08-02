3b3babb19723353f6a006f6cbe759ae10e6fa979
Nessrine88 <macherki.nessrine@yahoo.com>
2026-08-02 22:49:58 +0100

    place order component

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,5 @@
 import z from "zod";
-import { insertProductSchema, insertCartSchema, cartItemSchema, shippingAddressSchema } from "@/app/lib/validators";
+import { insertProductSchema, insertCartSchema, cartItemSchema, shippingAddressSchema, insertOrderItemSchema, insertOrderSchema } from "@/app/lib/validators";
 export type Product = z.infer <typeof insertProductSchema> & {
     id:string;
     rating:number;
@@ -10,4 +10,16 @@ export type Product = z.infer <typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+export type OrderItem = z.infer<typeof insertOrderItemSchema>
+export type Order = z.infer<typeof insertOrderSchema> & {
+    id: string;
+    createdAt: Date;
+    isPaid: Boolean;
+    paidAt: Date | null;
+    isDelivered: Boolean;
+    deliveredAt: Date | null;
+    orderItems: OrderItem[];
+    user: {name: string; email: string}
+
+}
 
